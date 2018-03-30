bb4eed791c4761051c685b25bf139467ec420d61
Martin Styk <martin.styk@gmail.com>
2018-03-30 13:01:09 +0200

    Monitor memory leaks using leak canary

---
+++ app/src/main/java/sk/momosi/carific/Carific.kt
@@ -0,0 +1,23 @@
+package sk.momosi.carific
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+/**
+ * @author Martin Styk
+ * @date 30.03.2018.
+ */
+class Carific: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        LeakCanary.install(this)
+    }
+}
