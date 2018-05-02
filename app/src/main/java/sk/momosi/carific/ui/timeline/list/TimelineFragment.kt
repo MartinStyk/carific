f60097a5c128caf9256f94c859b25e038aff440d
Martin Styk <mstyk@redhat.com>
2018-05-02 15:59:07 +0200

    Floating add button with multiple options

--- app/src/main/java/sk/momosi/carific/ui/timeline/list/TimelineFragment.kt
+++ app/src/main/java/sk/momosi/carific/ui/timeline/list/TimelineFragment.kt
@@ -7,11 +7,12 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CollapsingToolbarLayout
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.getbase.floatingactionbutton.FloatingActionButton
+import com.getbase.floatingactionbutton.FloatingActionsMenu
 import kotlinx.android.synthetic.main.fragment_timeline_list.*
 import sk.momosi.carific.R
 import sk.momosi.carific.databinding.FragmentTimelineListBinding
@@ -21,8 +22,8 @@ import sk.momosi.carific.ui.expense.edit.AddEditExpenseActivity
 import sk.momosi.carific.ui.fuel.edit.AddEditFuelActivity
 import sk.momosi.carific.view.recycler.decorations.ImageItemDecoration
 import sk.momosi.carific.view.recycler.decorations.RecyclerSectionItemDecoration
-import xyz.sangcomz.stickytimelineview.RoadItemDecoration
 import sk.momosi.carific.view.recycler.model.SectionInfo
+import xyz.sangcomz.stickytimelineview.RoadItemDecoration
 
 
 class TimelineFragment : Fragment() {
@@ -50,16 +51,12 @@ class TimelineFragment : Fragment() {
 
         setupList()
 
+        setupAddButton()
+
         activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = getString(R.string.title_timeline, getCar().name)
         activity?.findViewById<AppBarLayout>(R.id.app_bar)?.setExpanded(true, true)
 
         viewModel.init(getCar().id, getUser())
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        setupAddButton()
     }
 
     override fun onResume() {
@@ -68,14 +65,26 @@ class TimelineFragment : Fragment() {
     }
 
     private fun setupAddButton() {
-        val addBtn = activity?.findViewById<FloatingActionButton>(R.id.fab)
+        val fabMenu = activity?.findViewById<FloatingActionsMenu>(R.id.fab)
+        fabMenu?.let {
+            it.visibility=View.VISIBLE
+            it.collapse()
+        }
 
-        addBtn?.setOnClickListener {
+        activity?.findViewById<FloatingActionButton>(R.id.fab_fuel)?.setOnClickListener {
             val createIntent = Intent(context, AddEditFuelActivity::class.java)
             createIntent.putExtra(AddEditFuelActivity.ARG_CAR_ID, viewModel.carId)
             createIntent.putExtra(AddEditFuelActivity.ARG_USER, viewModel.user)
-
             startActivity(createIntent)
+            fabMenu?.toggle()
+        }
+
+        activity?.findViewById<FloatingActionButton>(R.id.fab_expense)?.setOnClickListener {
+            val createIntent = Intent(context, AddEditExpenseActivity::class.java)
+            createIntent.putExtra(AddEditExpenseActivity.ARG_CAR_ID, viewModel.carId)
+            createIntent.putExtra(AddEditExpenseActivity.ARG_USER, viewModel.user)
+            startActivity(createIntent)
+            fabMenu?.toggle()
         }
     }
 
@@ -99,6 +108,11 @@ class TimelineFragment : Fragment() {
         })
     }
 
+
+    override fun onDestroyView() {
+        activity?.findViewById<View>(R.id.fab)?.visibility = View.GONE
+        super.onDestroyView()
+    }
 
     private fun setupList() {
         fuel_list.adapter = TimelineListAdapter(viewModel = viewModel)
